8cac8ad38c0f25e687da3c07fb253d0249e05b48
Artem Dereviago <artemderev15@mail.ru>
2022-07-05 21:24:05 +0300

    Add palette component

---
+++ components/palette/Palette.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import css from './Palette.module.scss';
+
+function Palette() {
+  return (
+    <div className={css.paletteBlock}>
+      <div className={css.paletteBlockItem} />
+      <div className={css.paletteBlockItem} />
+      <div className={css.paletteBlockItem} />
+      <div className={css.paletteBlockItem} />
+      <div className={css.paletteBlockItem} />
+      <div className={css.paletteBlockItem} />
+      <div className={css.paletteBlockItem} />
+      <div className={css.paletteBlockItem} />
+      <div className={css.paletteBlockItem} />
+      <div className={css.paletteBlockItem} />
+    </div>
+  );
+}
+
+export default Palette;
